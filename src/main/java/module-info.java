cb8598493546db7c0c1ae6e7e9e3cf76bbf3035c
RobertMili <robertmilicevic3869@gmail.com>
2022-10-26 15:01:59 +0200

    drawing cirkle and recktangel work

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,6 @@ module com.example.labb3 {
 
     opens com.example.labb3 to javafx.fxml;
     exports com.example.labb3;
+    exports com.example.labb3.Shapes;
+    opens com.example.labb3.Shapes to javafx.fxml;
 }
